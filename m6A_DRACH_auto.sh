4302f66484ca9bba5d0cd1ed91916b5bb0e7c816
Mingzhe Cai 蔡明哲 <pyrus.cai@outlook.com>
2026-03-06 00:03:53 +0100

    Create m6A_DRACH_auto.sh

---
+++ m6A_DRACH_auto.sh
@@ -0,0 +1,119 @@
+export DORADO_MODELS_DIRECTORY="/home/vpm582/models"
+fasta3="/home/vpm582/ref3.fa"
+pod5_total="pod5_total.txt"
+pod5_virion="pod5_virion.txt"
+mod="m6A_DRACH"
+##############################################
+################### TOTAL ####################
+##############################################
+while IFS= read -r pod5_total; do
+    SAMPLE_NAME=$(basename "$(dirname "$(dirname "$pod5_total")")")
+
+    out_bam="calls_total_${SAMPLE_NAME}.bam"
+
+    echo "Processing sample: ${SAMPLE_NAME}"
+    echo "Input POD5 dir:   ${pod5_total}"
+    echo "Output BAM:       ${out_bam}"
+    echo
+
+    dorado basecaller sup "${pod5_total}/" \
+      -x cuda:0,1,2 \
+      --reference "${fasta3}" \
+      --recursive \
+      --verbose \
+      --modified-bases ${mod} \
+      > "${out_bam}"
+done < "${pod5_file}"
+##############################################
+################## VIRION ####################
+##############################################
+while IFS= read -r pod5_virion; do
+    SAMPLE_NAME=$(basename "$(dirname "$(dirname "$pod5_virion")")")
+
+    out_bam="calls_virion_${SAMPLE_NAME}.bam"
+
+    echo "Processing sample: ${SAMPLE_NAME}"
+    echo "Input POD5 dir:   ${pod5_virion}"
+    echo "Output BAM:       ${out_bam}"
+    echo
+
+    dorado basecaller sup "${pod5_virion}/" \
+      -x cuda:0,1,2 \
+      --reference "${fasta3}" \
+      --recursive \
+      --verbose \
+      --modified-bases ${mod} \
+      > "${out_bam}"
+done < "${pod5_file}"
+
+##############################################
+############## MODKIT VIRION #################
+##############################################
+for bamfile in calls_virion_*.bam; do
+    SAMPLE_NAME=$(basename "${bamfile}" .bam)
+    SAMPLE_NAME=${SAMPLE_NAME#calls_virion_}
+
+    SORTED_BAM="sorted_calls_virion_${SAMPLE_NAME}.bam"
+    PILEUP_BED="pileup_virion_${SAMPLE_NAME}.bed"
+    EXTRACT_BED="extract_virion_${SAMPLE_NAME}.bed"
+    
+    echo "Processing sample: ${SAMPLE_NAME}"
+    echo "Input BAM:         ${bamfile}"
+    echo "Sorted BAM:        ${SORTED_BAM}"
+    echo "Pileup BED:        ${PILEUP_BED}"
+    echo
+    
+    # 1. Get summary of reads
+    dorado summary "${bamfile}" > summary_reads_virion_${SAMPLE_NAME}.tsv
+    # 2. Sort the BAM
+    samtools sort "${bamfile}" -o "${SORTED_BAM}"
+    # 3. Index the sorted BAM
+    samtools index "${SORTED_BAM}"
+
+    # 4. Run modkit pileup
+    modkit pileup \
+        "${SORTED_BAM}" \
+        "${PILEUP_BED}" \
+        --reference /home/vpm582/ref.fa
+        --modified-bases m6A \
+        --log-filepath pileup.log
+
+    echo "Finished sample: ${SAMPLE_NAME}"
+    echo "----------------------------------------"
+done
+##############################################
+
+##############################################
+############## MODKIT TOTAL ##################
+##############################################
+for bamfile in calls_total_*.bam; do
+    SAMPLE_NAME=$(basename "${bamfile}" .bam)
+    SAMPLE_NAME=${SAMPLE_NAME#calls_total_}
+
+    SORTED_BAM="calls_total_${SAMPLE_NAME}_sorted.bam"
+    PILEUP_BED="pileup_total_${SAMPLE_NAME}.bed"
+    EXTRACT_BED="extract_total_${SAMPLE_NAME}.bed"
+    
+    echo "Processing sample: ${SAMPLE_NAME}"
+    echo "Input BAM:         ${bamfile}"
+    echo "Sorted BAM:        ${SORTED_BAM}"
+    echo "Pileup BED:        ${PILEUP_BED}"
+    echo
+    
+    # 1. Get summary of reads
+    dorado summary "${bamfile}" > summary_reads_total_${SAMPLE_NAME}.tsv
+    # 2. Sort the BAM
+    samtools sort "${bamfile}" -o "${SORTED_BAM}"
+    # 3. Index the sorted BAM
+    samtools index "${SORTED_BAM}"
+
+    # 4. Run modkit pileup
+    modkit pileup \
+        "${SORTED_BAM}" \
+        "${PILEUP_BED}" \
+        --modified-bases m6A \
+        --log-filepath pileup.log
+
+    echo "Finished sample: ${SAMPLE_NAME}"
+    echo "----------------------------------------"
+done
