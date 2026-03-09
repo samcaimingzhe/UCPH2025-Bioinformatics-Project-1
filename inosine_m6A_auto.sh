6fbaca5bd7135c2f1c15f03c42c670d2cae12d27
Mingzhe Cai 蔡明哲 <pyrus.cai@outlook.com>
2026-03-09 22:47:46 +0100

    Add mod variable to inosine_m6A_auto.sh

--- inosine_m6A_auto.sh
+++ inosine_m6A_auto.sh
@@ -2,6 +2,8 @@ export DORADO_MODELS_DIRECTORY="/home/vpm582/models"
 fasta3="/home/vpm582/ref3.fa"
 pod5_total="pod5_total.txt"
 pod5_virion="pod5_virion.txt"
+
+
 mod="inosine_m6A_2OmeA"
 ##############################################
 ################### TOTAL ####################
